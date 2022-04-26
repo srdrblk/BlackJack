5bc8930bd50a8193d9e735deb642728185aa5a10
Serdar Bilek <serdar.bilek@softtech.com.tr>
2022-04-26 11:08:26 +0300

    rev

--- BlackJack/Startup.cs
+++ BlackJack/Startup.cs
@@ -47,7 +47,7 @@ namespace BlackJack
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
+
             app.UseStaticFiles();
 
             app.UseRouting();
